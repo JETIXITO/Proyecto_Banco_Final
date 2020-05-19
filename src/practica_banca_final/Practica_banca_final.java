1a3da15b430248e224f5d7e4c40493874e93d919
Denis Marian Manta <Denis Marian Manta@DESKTOP-75UKFLT>
2020-05-19 20:08:05 +0200

    Merge origin/develop
    Conflicts:
    	src/inicioapp/InicioApp.java

---
+++ src/practica_banca_final/Practica_banca_final.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica_banca_final;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Denis Marian Manta
+ */
+public class Practica_banca_final {
+
+    static Scanner sc = new Scanner(System.in);
+    static ArrayList<Datos_Cliente> Cuentas = new ArrayList<Datos_Cliente>();
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
